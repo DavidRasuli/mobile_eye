03f1dc97ecdf655f66776af90f83658d771dcc27
David Rasuli <68276145+DavidRasuli@users.noreply.github.com>
2024-02-06 12:52:03 +0200

    Update simple_select_query_builder.py

--- proof_of_protability/simple_select_query_builder.py
+++ proof_of_protability/simple_select_query_builder.py
@@ -1,1 +1,22 @@
 
+'''
+The idea behind this class is to prove that the query builder
+structure can work well for other objects for other
+purposes within this solution. Limiting to 10 records - hardcoded.
+'''
+
+
+class SelectQueryBuilder:
+    def __init__(self, table):
+        self._table = table
+        self._columns = []
+
+    def add_column(self, column):
+        self._columns.append(column)
+
+    def build(self) -> str:
+        if not self._columns:
+            raise ValueError("No columns added. Please use add_column before building the query.")
+
+        select_clause = f"SELECT {', '.join(self._columns)} FROM {self._table} limit 10"
+        return select_clause
